3a0825be44c4de589c8f2e755201e38fde5f72e3
comancheace <jmnalves@hotmail.com>
2024-02-13 20:58:01 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Galerias de Arte</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)" href="splash_screens/iPhone_15_Pro_Max__iPhone_15_Plus__iPhone_14_Pro_Max_landscape.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)" href="splash_screens/iPhone_15_Pro__iPhone_15__iPhone_14_Pro_landscape.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)" href="splash_screens/iPhone_14_Plus__iPhone_13_Pro_Max__iPhone_12_Pro_Max_landscape.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)" href="splash_screens/iPhone_14__iPhone_13_Pro__iPhone_13__iPhone_12_Pro__iPhone_12_landscape.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)" href="splash_screens/iPhone_13_mini__iPhone_12_mini__iPhone_11_Pro__iPhone_XS__iPhone_X_landscape.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)" href="splash_screens/iPhone_11_Pro_Max__iPhone_XS_Max_landscape.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" href="splash_screens/iPhone_11__iPhone_XR_landscape.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)" href="splash_screens/iPhone_8_Plus__iPhone_7_Plus__iPhone_6s_Plus__iPhone_6_Plus_landscape.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" href="splash_screens/iPhone_8__iPhone_7__iPhone_6s__iPhone_6__4.7__iPhone_SE_landscape.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" href="splash_screens/4__iPhone_SE__iPod_touch_5th_generation_and_later_landscape.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" href="splash_screens/12.9__iPad_Pro_landscape.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" href="splash_screens/11__iPad_Pro__10.5__iPad_Pro_landscape.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 820px) and (device-height: 1180px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" href="splash_screens/10.9__iPad_Air_landscape.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" href="splash_screens/10.5__iPad_Air_landscape.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" href="splash_screens/10.2__iPad_landscape.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" href="splash_screens/9.7__iPad_Pro__7.9__iPad_mini__9.7__iPad_Air__9.7__iPad_landscape.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 744px) and (device-height: 1133px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" href="splash_screens/8.3__iPad_Mini_landscape.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" href="splash_screens/iPhone_15_Pro_Max__iPhone_15_Plus__iPhone_14_Pro_Max_portrait.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" href="splash_screens/iPhone_15_Pro__iPhone_15__iPhone_14_Pro_portrait.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" href="splash_screens/iPhone_14_Plus__iPhone_13_Pro_Max__iPhone_12_Pro_Max_portrait.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" href="splash_screens/iPhone_14__iPhone_13_Pro__iPhone_13__iPhone_12_Pro__iPhone_12_portrait.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" href="splash_screens/iPhone_13_mini__iPhone_12_mini__iPhone_11_Pro__iPhone_XS__iPhone_X_portrait.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" href="splash_screens/iPhone_11_Pro_Max__iPhone_XS_Max_portrait.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="splash_screens/iPhone_11__iPhone_XR_portrait.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" href="splash_screens/iPhone_8_Plus__iPhone_7_Plus__iPhone_6s_Plus__iPhone_6_Plus_portrait.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="splash_screens/iPhone_8__iPhone_7__iPhone_6s__iPhone_6__4.7__iPhone_SE_portrait.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="splash_screens/4__iPhone_SE__iPod_touch_5th_generation_and_later_portrait.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="splash_screens/12.9__iPad_Pro_portrait.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="splash_screens/11__iPad_Pro__10.5__iPad_Pro_portrait.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 820px) and (device-height: 1180px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="splash_screens/10.9__iPad_Air_portrait.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="splash_screens/10.5__iPad_Air_portrait.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="splash_screens/10.2__iPad_portrait.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="splash_screens/9.7__iPad_Pro__7.9__iPad_mini__9.7__iPad_Air__9.7__iPad_portrait.png">
+<link rel="apple-touch-startup-image" media="screen and (device-width: 744px) and (device-height: 1133px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="splash_screens/8.3__iPad_Mini_portrait.png">
+<meta property="og:title" content="Galerias de Arte">
+<meta property="og:type" content="website" />
+<meta property="og:url" content="https://galeriajoaoalves.pt/galeria_db/" />
+<meta property="og:image" content="https://galeriajoaoalves.pt/galeria_db/img/logo.jpg" />
+<meta property="og:description" content="Galerias de Arte" />
+<link rel="icon" href="img/webapp_32x32.jpg" sizes="32x32" />
+<link rel="icon" href="img/webapp_192x192.jpg" sizes="192x192" />
+<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+<link rel="manifest" href="js/site.webmanifest">
+<meta name="msapplication-TileImage" content="img/logo.jpg" />
+<link rel="shortcut icon" href="favicon.ico">
+<link rel="stylesheet" href="css/jquery.mobile-1.4.5.min.css">
+<link rel="stylesheet" href="css/jqm-demos.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+<script src="js/jquery.js"></script>
+<script src="js/index.js"></script>
+<script src="js/jquery.mobile-1.4.5.min.js"></script>
+<script>
+		$( document ).on( "pagecreate", "#demo-page", function() {
+
+			$( ".Pinturas" ).on( "click", function() {
+				var target = $( this ),
+					brand = target.find( "h2" ).html(),
+					model = target.find( "p" ).html(),
+					short = target.attr( "id" ),
+					closebtn = '<a href="#" data-rel="back" class="ui-btn ui-corner-all ui-btn-a ui-icon-delete ui-btn-icon-notext ui-btn-right">Close</a>',
+					header = '<div data-role="header"><h2>' + brand + ' ' + model + '</h2></div>',
+					img = '<img src="img/' + short + '.jpg" alt="' + brand + '" class="photo">',
+					popup = '<div data-role="popup" id="popup-' + short + '" data-short="' + short +'" data-theme="none" data-overlay-theme="a" data-corners="false" data-tolerance="15"></div>';
+
+				// Create the popup.
+				$( header )
+					.appendTo( $( popup )
+						.appendTo( $.mobile.activePage )
+						.popup() )
+					.toolbar()
+					.before( closebtn )
+					.after( img );
+
+				// Wait with opening the popup until the popup image has been loaded in the DOM.
+				// This ensures the popup gets the correct size and position
+				$( ".photo", "#popup-" + short ).load(function() {
+					// Open the popup
+					$( "#popup-" + short ).popup( "open" );
+
+					// Clear the fallback
+					clearTimeout( fallback );
+				});
+
+				// Fallback in case the browser doesn't fire a load event
+				var fallback = setTimeout(function() {
+					$( "#popup-" + short ).popup( "open" );
+				}, 2000);
+			});
+
+			// Set a max-height to make large images shrink to fit the screen.
+			$( document ).on( "popupbeforeposition", ".ui-popup", function() {
+				var image = $( this ).children( "img" ),
+					height = image.height(),
+					width = image.width();
+
+				// Set height and width attribute of the image
+				$( this ).attr({ "height": height, "width": width });
+
+				// 68px: 2 * 15px for top/bottom tolerance, 38px for the header.
+				var maxHeight = $( window ).height() - 68 + "px";
+
+				$( "img.photo", this ).css( "max-height", maxHeight );
+			});
+
+			// Remove the popup after it has been closed to manage DOM size
+			$( document ).on( "popupafterclose", ".ui-popup", function() {
+				$( this ).remove();
+			});
+		});
+</script>
+<style>
+		/* Reduce the height of the header on smaller screens. */
+		@media all and (max-width: 48em){
+			.ui-popup .ui-title {
+				font-size: .75em;
+			}
+		}
+</style>
+</head>
+<body>
+<div data-role="page" id="demo-intro" class="jqm-demos">
+
+	<div data-role="header" class="jqm-header">
+	<h1>Galerias de Arte</h1>	
+    <h2><a href="#" title="Galerias de Arte"><img src="img/logo2.jpg" alt="Galerias de Arte"></a></h2>
+		<a href="#" class="jqm-navmenu-link ui-btn ui-btn-icon-notext ui-corner-all ui-icon-bars ui-nodisc-icon ui-alt-icon ui-btn-left">Menu</a>
+		<a href="#" class="jqm-search-link ui-btn ui-btn-icon-notext ui-corner-all ui-icon-search ui-nodisc-icon ui-alt-icon ui-btn-right">Search</a>
+	</div><!-- /header -->
+
+	<div role="main" class="ui-content jqm-content">
+
+    <form class="ui-filterable">
+    <input id="filterBasic-input" data-type="search">
+</form>
+<ul data-role="listview" data-filter="true" data-input="#filterBasic-input">
+
+<?php
+
+include 'db_connect.php';
+
+$sql = "SELECT nome, email FROM galerias";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+		//echo '<div title = "'.$row['category_title'].'">';
+		echo '<li><h4>Nome: </h4>'.$row['nome'].'</li>';
+		echo '<li><h4>Email: </h4>'.$row['email'].'</li>';
+	}
+} else {
+    echo "0 results";
+}
+
+?>
+</ul>
+
+	</div><!-- /content -->
+    <!-- /left-panel -->
+	    <div data-role="panel" class="jqm-navmenu-panel" data-position="left" data-display="overlay" data-theme="a">
+	    	<ul class="jqm-list ui-alt-icon ui-nodisc-icon">
+                <li data-filtertext="demos homepage" data-icon="heart"><a href="#demo-page">Portfolio</a></li>
+                <li data-filtertext="introduction overview getting started"><a href="#About" data-ajax="false">About</a></li>
+		     </ul>
+		</div><!-- /left-panel -->
+
+
+	<div data-role="footer" data-position="fixed" data-tap-toggle="false" class="jqm-footer">
+        <p>Base de dados de Galerias de Arte</p>
+		<p>Copyright 2024 galeriajoaoalves.pt</p>
+	</div><!-- /footer -->
+		<!-- /right_panel -->
+        <div data-role="panel" class="jqm-search-panel" data-position="right" data-display="overlay" data-theme="a">
+		<div class="jqm-search">
+			<ul class="jqm-list" data-filter-placeholder="Search demos..." data-filter-reveal="true">
+            <?php
+                include 'db_connect.php';
+
+                $sql = "SELECT nome, email FROM galerias";
+                $result = mysqli_query($conn, $sql);
+
+                if (mysqli_num_rows($result) > 0) {
+                    // output data of each row
+                    while($row = mysqli_fetch_assoc($result)) {
+                        //echo '<div title = "'.$row['category_title'].'">';
+                        echo '"Nome: <li data-filtertext="'.$row['nome'].'">"'.$row['nome'].'"</li>"';
+                        echo '"Nome: <li data-filtertext="'.$row['email'].'">"'.$row['email'].'"</li>"';
+                    }
+                } else {
+                    echo "0 results";
+                }
+
+                ?>    
+        </ul>
+	
+	</div>
+</li>
+
+
+
+			</ul>
+		</div>
+	</div><!-- /right-panel -->
+
+
+</div><!-- /page -->
+
+<div data-role="page" id="demo-page" data-title="Pinturas">
+
+    <div data-role="header" data-theme="b">
+        <a href="#demo-intro" data-rel="back" data-icon="arrow-l" data-iconpos="notext" data-shadow="false" data-icon-shadow="false">Back</a>
+        <h1>Pinturas</h1>
+    </div><!-- /header -->
+
+    <div role="main" class="ui-content">
+
+        <ul data-role="listview">
+            
+            <li><a href="#" class="Pinturas" id="1"><img src="img/1.jpg" alt="Pintura 1"><h2>Pintura 1</h2><p>Pulchritudo Florum</p></a></li>
+            <li><a href="#" class="Pinturas" id="2"><img src="img/2.jpg" alt="Pintura 2"><h2>Pintura 2</h2><p>Pulchritudo Florum</p></a></li>
+            <li><a href="#" class="Pinturas" id="3"><img src="img/3.jpg" alt="Pintura 3"><h2>Pintura 3</h2><p>Pulchritudo Florum</p></a></li>
+            <li><a href="#" class="Pinturas" id="4"><img src="img/4.jpg" alt="Pintura 4"><h2>Pintura 4</h2><p>Pulchritudo Florum</p></a></li>
+            <li><a href="#" class="Pinturas" id="5"><img src="img/5.jpg" alt="Pintura 5"><h2>Pintura 5</h2><p>Pulchritudo Florum</p></a></li>
+            <li><a href="#" class="Pinturas" id="7"><img src="img/7.jpg" alt="Pintura 6"><h2>Pintura 6</h2><p>Pulchritudo Florum</p></a></li>
+            <li><a href="#" class="Pinturas" id="8"><img src="img/8.jpg" alt="Pintura 7"><h2>Pintura 7</h2><p>Pulchritudo Florum</p></a></li>
+            <li><a href="#" class="Pinturas" id="9"><img src="img/9.jpg" alt="Pintura 8"><h2>Pintura 8</h2><p>Pulchritudo Florum</p></a></li>
+            <li><a href="#" class="Pinturas" id="10"><img src="img/10.jpg" alt="Pintura 9"><h2>Pintura 9</h2><p>Pulchritudo Florum</p></a></li>
+            <li><a href="#" class="Pinturas" id="6"><img src="img/6.jpg" alt="Poster"><h2>Poster</h2><p>Pulchritudo Florum</p></a></li>
+        </ul>
+
+    </div><!-- /content -->
+
+</div><!-- /page -->
+
+
+<div data-role="page" id="About" data-title="About">
+
+    <div data-role="header" data-theme="b">
+        <a href="#demo-intro" data-rel="back" data-icon="arrow-l" data-iconpos="notext" data-shadow="false" data-icon-shadow="false">Back</a>
+        <h1>About</h1>
+    </div><!-- /header -->
+
+    <div role="main" class="ui-content">
+
+    <h1>About</h1>
+    <p>Made by: galeriajoaoalves.pt</p>
+    <p>With PHP, MySQL, HTML5 and JavaScript</p>    
+
+    </div><!-- /content -->
+
+</div><!-- /page -->
+
+</body>
+</html>
