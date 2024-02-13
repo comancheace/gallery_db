a66bf25fa6ea15803a2a8e3bb88767dd7cf68b29
comancheace <jmnalves@hotmail.com>
2024-02-13 21:59:23 +0000

    Update index.php

--- index.php
+++ index.php
@@ -127,8 +127,8 @@
 <div data-role="page" id="demo-intro" class="jqm-demos">
 
 	<div data-role="header" class="jqm-header">
-	<h1>Galerias de Arte</h1>	
-    <h2><a href="#" title="Galerias de Arte"><img src="img/logo2.jpg" alt="Galerias de Arte"></a></h2>
+		<h1>Galerias de Arte</h1>	
+    		<h2><a href="#" title="Galerias de Arte"><img src="img/logo2.jpg" alt="Galerias de Arte"></a></h2>
 		<a href="#" class="jqm-navmenu-link ui-btn ui-btn-icon-notext ui-corner-all ui-icon-bars ui-nodisc-icon ui-alt-icon ui-btn-left">Menu</a>
 		<a href="#" class="jqm-search-link ui-btn ui-btn-icon-notext ui-corner-all ui-icon-search ui-nodisc-icon ui-alt-icon ui-btn-right">Search</a>
 	</div><!-- /header -->
